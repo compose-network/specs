c79c07abe45474364ca13330865793319695927f
Shota Silagadze <shota@ssvlabs.io>
2025-12-14 01:16:40 +0400

    fix start instance issue

--- compose/sbcp/sequencer.go
+++ compose/sbcp/sequencer.go
@@ -3,7 +3,6 @@ package sbcp
 import (
 	"context"
 	"errors"
-	"fmt"
 	"sync"
 
 	"github.com/compose-network/specs/compose"
@@ -15,7 +14,7 @@ var (
 		"block number to be sealed does not match the current block number",
 	)
 	ErrBlockAlreadyOpen         = errors.New("there is already an open block")
-	ErrBlockNotSequential       = errors.New("block number is not sequential-1")
+	ErrBlockNotSequential       = errors.New("block number is not sequential")
 	NoPendingBlock              = errors.New("no pending block")
 	ErrActiveInstanceExists     = errors.New("there is already an active instance")
 	ErrNoActiveInstance         = errors.New("no active instance")
@@ -175,16 +174,13 @@ func (s *sequencer) startSettlement(ctx context.Context, periodID compose.Period
 
 // BeginBlock is a hook called at the start of a new L2 block.
 func (s *sequencer) BeginBlock(blockNumber BlockNumber) error {
-	fmt.Println("shota BeginBlock", blockNumber)
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	fmt.Println("shota BeginBlock 2", blockNumber)
 	if s.PendingBlock != nil {
 		return ErrBlockAlreadyOpen
 	}
 
 	if s.Head != 0 && blockNumber != s.Head+1 {
-		fmt.Println("shota -- ErrBlockNotSequential", blockNumber, s.Head)
 		return ErrBlockNotSequential
 	}
 
@@ -279,7 +275,6 @@ func (s *sequencer) EndBlock(ctx context.Context, b BlockHeader) error {
 	settlementPeriod := s.PeriodID - 1
 	settlementSuperblock := s.TargetSuperblockNumber - 1
 
-	fmt.Println("shota Endblock")
 	s.PendingBlock = nil
 	s.Head = b.Number
 
@@ -332,7 +327,6 @@ func (s *sequencer) Rollback(
 	}
 
 	// Discard current block and active instance
-	fmt.Println("shota Endblock")
 	s.PendingBlock = nil
 	s.ActiveInstanceID = nil
 	s.Head = s.SettledState.BlockHeader.Number
