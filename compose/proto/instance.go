a28f03d3d7721bf143ee895f09d0676d5a9de186
Karol Chojnowski <karolchojnowski95@gmail.com>
2026-01-15 21:49:25 +0100

    proto: add helper methods for working with proto types
    Split into logical files:
    - instance.go: InstanceIDHex() for StartInstance, Vote, Decided
    - request.go: TransactionsByChain() for XTRequest
    
    # Conflicts:
    #	compose/proto/instance.go
    #	compose/proto/instance_test.go
    
    # Conflicts:
    #	compose/proto/request.go

--- compose/proto/instance.go
+++ compose/proto/instance.go
@@ -2,6 +2,8 @@ package proto
 
 import "encoding/hex"
 
+// InstanceIDHex returns the instance ID as a lowercase hex string.
+// Returns empty string if receiver is nil.
 func (x *StartInstance) InstanceIDHex() string {
 	if x == nil {
 		return ""
@@ -9,6 +11,8 @@ func (x *StartInstance) InstanceIDHex() string {
 	return hex.EncodeToString(x.GetInstanceId())
 }
 
+// InstanceIDHex returns the instance ID as a lowercase hex string.
+// Returns empty string if receiver is nil.
 func (x *Vote) InstanceIDHex() string {
 	if x == nil {
 		return ""
@@ -16,6 +20,8 @@ func (x *Vote) InstanceIDHex() string {
 	return hex.EncodeToString(x.GetInstanceId())
 }
 
+// InstanceIDHex returns the instance ID as a lowercase hex string.
+// Returns empty string if receiver is nil.
 func (x *Decided) InstanceIDHex() string {
 	if x == nil {
 		return ""
